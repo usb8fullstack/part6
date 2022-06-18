8389fa4e46b90a96ef561ddddd6617469de97fb7
Hieu Nguyen <nth937@gmail.com>
2022-06-18 23:12:43 +0300

    ex6.15: asynchronous action creators - Redux Thunk library

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -5,15 +5,17 @@ import Notification from './components/Notification'
 
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import anecService from './services/anecs'
-import { setAnecs } from './reducers/anecdoteReducer'
+// import anecService from './services/anecs'
+// import { setAnecs } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    anecService
-      .getAll()
-      .then(anecs => dispatch(setAnecs(anecs)))
+    // anecService
+    //   .getAll()
+    //   .then(anecs => dispatch(setAnecs(anecs)))
+    dispatch(initializeAnecdotes()) 
   }, [dispatch])
 
   return (
