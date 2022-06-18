95e7fdefdde64881c7d9c97e97c365528b1ed796
Hieu Nguyen <nth937@gmail.com>
2022-06-18 16:51:36 +0300

    ex6.12: step1: create filter component

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,15 +1,17 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import Filter from './components/Filter'
 import Notification from './components/Notification'
 
 const App = () => {
   return (
     <div>
+      <Filter />
       <Notification />
-      <h2>create new</h2>
-      <AnecdoteForm />
       <h2>Anecdotes</h2>
       <AnecdoteList />
+      <h2>create new</h2>
+      <AnecdoteForm />
     </div>
   )
 }
